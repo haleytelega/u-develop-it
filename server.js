3fe6cb8c2e365d28f2381b5e5142d185be1bc68a
Haley Telega <haleytelega@outlook.com>
2022-05-02 21:54:54 -0400

    added application to mysql db

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var express = require('express');
 const PORT = process.env.PORT || 3001;
 var app = express();
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const mysql = require('mysql2');
+//connect the application to the MySQL db
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'BlueTowelCats9*',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+    );
+
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
